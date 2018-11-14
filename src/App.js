1e7a01563ba91a2d209895f309fea11e557bfae2
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-14 18:44:06 -0500

    Interfaz Grafica

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Row, Col, Button } from 'react-bootstrap'
+import posts from './posts'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allPosts: posts,
+      posts: posts
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid>
+        <Row className="">
+          <Col md={8} mdOffset={2} >
+            <h1>Blog posts populares</h1>
+          <div className="general-grid">
+            <p>Orden: 
+              <Button>Ascendente</Button>
+              <Button>Descendente</Button>
+            </p>
+          </div>
+           {this.state.posts.map((post, index) =>  
+            <Row className="posts-grid">
+              <Col md={4}><a href={post.url}><img className="image" src={ post.post_image_url } /></a></Col>
+              <Col className="votes" md={1}>
+                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true" />
+                {post.votes}
+                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" />
+              </Col>
+              <Col md={7}><a href={post.url}><p>{ post.title }</p></a><p>{ post.description }</p><p className="writtenby">Escrito por: <img className="authoravatar" src={ post.writer_avatar_url } /></p></Col>
+            </Row>
+            )}
+          </Col>
+        </Row>
+    </Grid>
     );
   }
 }
