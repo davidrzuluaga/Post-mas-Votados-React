eeab8c7c139b93b6cde5bc46cf4bcc733b041bcd
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-18 21:58:10 -0500

    Voting system

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allPosts: posts,
       posts: posts
     }
   }
+  voteArrows = (e) => {
+    const index = this.state.posts.map(function(e) { return e.id; }).indexOf(Math.floor(e.target.id))
+    var postVotes = this.state.posts[index].votes
+    e.target.classList.contains("glyphicon-triangle-top") ? postVotes += 1 : postVotes -= 1
+    this.setState({
+      tasks: this.state.posts.map((post, i) =>
+        i === index ? {votes: post.votes = postVotes } : post
+      )
+    })
+  }
+  
   render() {
     return (
       <Grid>
@@ -24,14 +34,14 @@ class App extends Component {
             </p>
           </div>
            {this.state.posts.map((post, index) =>  
-            <Row className="posts-grid">
-              <Col md={4}><a href={post.url}><img className="image" src={ post.post_image_url } /></a></Col>
+            <Row className="posts-grid" key={post.id}>
+              <Col md={4}><a href={post.url}><img alt={ post.id } className="image" src={ post.post_image_url } /></a></Col>
               <Col className="votes" md={1}>
-                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true" />
+                <span onClick={this.voteArrows} id={post.id} className="glyphicon glyphicon-triangle-top arrow" aria-hidden="true" />
                 {post.votes}
-                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" />
+                <span onClick={this.voteArrows} id={post.id} className="glyphicon glyphicon-triangle-bottom arrow" aria-hidden="true" />
               </Col>
-              <Col md={7}><a href={post.url}><p>{ post.title }</p></a><p>{ post.description }</p><p className="writtenby">Escrito por: <img className="authoravatar" src={ post.writer_avatar_url } /></p></Col>
+              <Col md={7}><a href={post.url}><p>{ post.title }</p></a><p>{ post.description }</p><p className="writtenby">Escrito por: <img alt={ post.id } className="authoravatar" src={ post.writer_avatar_url } /></p></Col>
             </Row>
             )}
           </Col>
