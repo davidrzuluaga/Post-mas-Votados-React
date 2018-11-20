e3e4a4cca4d27f09de71240f1ba4f0940f6c731b
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-20 14:32:34 -0500

    Updates

---
+++ precache-manifest.b2884bb85061a3d58e2622c2b7c40804.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f63f2f77c44d11e29b5d",
+    "url": "/Post-mas-Votados-React/static/js/runtime~main.f63f2f77.js"
+  },
+  {
+    "revision": "5dfb89866c397ad2667a",
+    "url": "/Post-mas-Votados-React/static/js/main.5dfb8986.chunk.js"
+  },
+  {
+    "revision": "bac22553fd34c9543f26",
+    "url": "/Post-mas-Votados-React/static/js/1.bac22553.chunk.js"
+  },
+  {
+    "revision": "5dfb89866c397ad2667a",
+    "url": "/Post-mas-Votados-React/static/css/main.de2bf24e.chunk.css"
+  },
+  {
+    "revision": "f888339b24de5a6e34e82a08fb8c4140",
+    "url": "/Post-mas-Votados-React/index.html"
+  }
+];
